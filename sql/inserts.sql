755931f00fe6670ce6cbee4bccc839f5e70ae4cb
Kajlongero <dev.kajlongero@gmail.com>
2025-07-16 22:47:44 -0400

    feat: upgraded readability on inserts sql

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -1,4 +1,24 @@
-INSERT INTO roles (name) VALUES ('OWNER'), ('ADMINISTRATOR'), ('USER');
+INSERT INTO roles (name) VALUES 
+  ('OWNER'), 
+  ('ADMINISTRATOR'), 
+  ('USER');
 
-INSERT INTO objects (name) VALUES ('AuthenticationService'), ('AuthorizationService'), ('ReportsService');
-INSERT INTO methods (name, object_id) VALUES ('Login', 1), ('Signup', 1), ('RecoverPassword', 1), ('ResetPassword', 1), ('ConfirmAccount'), ('RefreshToken', 1), ('UserHasPermission', 2), ('ValidateSession', 2), ('GenerateCSV', 3), ('GenerateHTML', 3), ('GenerateXLSX', 3), ('GeneratePDF', 3), ('GenerateDOCX', 3);
+INSERT INTO objects (name) VALUES 
+  ('AuthenticationService'), 
+  ('AuthorizationService'), 
+  ('ReportsService');
+
+INSERT INTO methods (name, object_id) VALUES 
+  ('Login', 1), 
+  ('Signup', 1), 
+  ('RecoverPassword', 1),
+  ('ResetPassword', 1),
+  ('ConfirmAccount'),
+  ('RefreshToken', 1),
+  ('UserHasPermission', 2),
+  ('ValidateSession', 2),
+  ('GenerateCSV', 3),
+  ('GenerateHTML', 3),
+  ('GenerateXLSX', 3),
+  ('GeneratePDF', 3),
+  ('GenerateDOCX', 3);
