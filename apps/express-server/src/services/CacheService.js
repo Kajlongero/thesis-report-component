3a5a858336ef4a413d466a57489ba61beaf7bd6b
Kajlongero <dev.kajlongero@gmail.com>
2025-07-16 20:08:22 -0400

    feat: cache service to store db data and instances data of classes and methods

--- apps/express-server/src/services/CacheService.js
+++ apps/express-server/src/services/CacheService.js
@@ -1,3 +1,12 @@
-class CacheService {}
+class CacheService {
+  constructor() {}
+
+  static getInstance(id, connectionString, engine) {
+    if (!this.instances.has(id)) {
+      this.instances.set(id, new DBComponent(connectionString, engine));
+    }
+    return this.instances.get(id);
+  }
+}
 
 module.exports = CacheService;
