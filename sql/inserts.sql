c9838fe50bc15dcc274a35ccf73ca3270ab8512c
Kajlongero <dev.kajlongero@gmail.com>
2025-07-16 20:13:54 -0400

    feat: added more methods available

--- sql/inserts.sql
+++ sql/inserts.sql
@@ -1,4 +1,4 @@
 INSERT INTO roles (name) VALUES ('OWNER'), ('ADMINISTRATOR'), ('USER');
 
-INSERT INTO objects (name) VALUES ('AuthenticationService'), ('AuthorizationService');
-INSERT INTO methods (name, object_id) VALUES ('Login', 1), ('Signup', 1), ('RecoverPassword', 1), ('ResetPassword', 1), ('ConfirmAccount'), ('UserHasPermission', 2);
+INSERT INTO objects (name) VALUES ('AuthenticationService'), ('AuthorizationService'), ('ReportsService');
+INSERT INTO methods (name, object_id) VALUES ('Login', 1), ('Signup', 1), ('RecoverPassword', 1), ('ResetPassword', 1), ('ConfirmAccount'), ('RefreshToken', 1), ('UserHasPermission', 2), ('ValidateSession', 2), ('GenerateCSV', 3), ('GenerateHTML', 3), ('GenerateXLSX', 3), ('GeneratePDF', 3), ('GenerateDOCX', 3);
