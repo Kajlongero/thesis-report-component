84123de40c4ae1470b649a3b208c3acc487e34de
Kajlongero <dev.kajlongero@gmail.com>
2025-07-18 13:13:44 -0400

    feat: AuthenticationService with only methods and structure

--- apps/express-server/src/services/AuthenticationService.js
+++ apps/express-server/src/services/AuthenticationService.js
@@ -1,5 +1,9 @@
 class AuthenticationService {
-  async Login(req, res, data) {}
+  async Login(req, res, data) {
+    console.log("Hello!!");
+
+    res.statusCode = 202;
+  }
   async Signup(req, res, data) {}
   async ResetPassword(req, res, data) {}
   async ConfirmAccount(req, res, data) {}
