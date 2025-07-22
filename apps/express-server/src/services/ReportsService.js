fd30016c61f8c958846079d9bfd898de7a43af0a
Kajlongero <dev.kajlongero@gmail.com>
2025-07-22 04:33:31 -0400

    feat: structure for generate reports

--- apps/express-server/src/services/ReportsService.js
+++ apps/express-server/src/services/ReportsService.js
@@ -1,3 +1,9 @@
-class ReportsService {}
+class ReportsService {
+  async GeneratePDF(req, res, params) {}
+  async GenerateCSV(req, res, params) {}
+  async GenerateHTML(req, res, params) {}
+  async GenerateDOCX(req, res, params) {}
+  async GeneratePDF(req, res, params) {}
+}
 
 module.exports = ReportsService;
