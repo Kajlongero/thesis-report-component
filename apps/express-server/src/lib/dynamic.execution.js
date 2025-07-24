76dcbfc7715f27a9ace339bdde2cca4508901d07
Kajlongero <dev.kajlongero@gmail.com>
2025-07-24 13:12:35 -0400

    refactor: updated dynamicExecuteMethod due to cache service refactor

--- apps/express-server/src/lib/dynamic.execution.js
+++ apps/express-server/src/lib/dynamic.execution.js
@@ -1,8 +1,10 @@
 const url = require("url");
 const path = require("path");
-const CacheLoader = require("./cache.loader");
 
-const cacheLoader = new CacheLoader();
+const CACHE_KEYS = require("../constants/cache");
+
+const CacheService = require("./cache");
+const cacheLoader = new CacheService();
 
 const SERVICES_BASE_PATH = path.join(
   process.cwd(),
@@ -12,28 +14,55 @@ const SERVICES_BASE_PATH = path.join(
   "services"
 );
 
+const getObjectAndMethod = (body) => {
+  const { tx, params, security } = body;
+
+  const transactions = cacheLoader.findInCache(CACHE_KEYS.TRANSACTIONS);
+
+  const data = transactions.get(tx);
+  if (!data) throw new notFound("Transaction id invalid");
+
+  const [_, objectName, methodName] = data.split("_");
+
+  const obj = {
+    objectName,
+    methodName,
+    security,
+    params,
+  };
+
+  return obj;
+};
+
 const dynamicExecuteMethod = async (req, res, body) => {
-  const method = body.methodName;
-  const object = body.objectName;
-  const params = body.params;
+  const { objectName, methodName, params } = getObjectAndMethod(body);
 
   let serviceInstance;
 
-  serviceInstance = cacheLoader.findInstance(object);
+  if (!cacheLoader.hasInCache(CACHE_KEYS.OBJECT_METHODS_INSTANCES))
+    cacheLoader.addToCache(CACHE_KEYS.OBJECT_METHODS_INSTANCES, new Map());
+
+  const cache = cacheLoader.findInCache(CACHE_KEYS.OBJECT_METHODS_INSTANCES);
+
+  serviceInstance = cache.get(objectName);
 
   if (!serviceInstance) {
-    const servicePath = path.join(SERVICES_BASE_PATH, `${object}.js`);
+    const servicePath = path.join(SERVICES_BASE_PATH, `${objectName}.js`);
 
     const { default: ServiceClass } = await import(
       url.pathToFileURL(servicePath).href
     );
     serviceInstance = new ServiceClass();
 
-    cacheLoader.saveInstance(object, serviceInstance);
+    const instancesMap = cacheLoader.findInCache(
+      CACHE_KEYS.OBJECT_METHODS_INSTANCES
+    );
+
+    instancesMap.set(objectName, serviceInstance);
   }
 
-  const executedMethod = await serviceInstance[method](req, res, params);
+  const executedMethod = await serviceInstance[methodName](req, res, params);
   return executedMethod;
 };
 
-module.exports = dynamicExecuteMethod;
+module.exports = { getObjectAndMethod, dynamicExecuteMethod };
