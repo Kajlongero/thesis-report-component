d13d67090dc2d160c7e638bd204e7b77e83f4e44
Kajlongero <dev.kajlongero@gmail.com>
2025-07-25 19:14:40 -0400

    feat: restructure main.tsx to include ThemeProvider and QueryClientProvider; integrate ThemeToggle component

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,15 +1,28 @@
-import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import { ThemeProvider } from "./context/ThemeContext.tsx";
+import { AuthContextProvider } from "./context/AuthContext.tsx";
 
 import App from "./App.tsx";
 
 import "./index.css";
-import { AuthContextProvider } from "./context/AuthContext.tsx";
+import { ThemeToggle } from "./components/Themes/ToggleTheme.tsx";
+
+const client = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    <QueryClientProvider client={client}>
+      <AuthContextProvider>
+        <ThemeProvider>
+          <ThemeToggle />
+          <ToastContainer />
+          <App />
+        </ThemeProvider>
+      </AuthContextProvider>
+    </QueryClientProvider>
   </BrowserRouter>
 );
