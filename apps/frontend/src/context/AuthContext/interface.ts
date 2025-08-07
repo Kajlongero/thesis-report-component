8bf5a7cb5d371569d1e2d6fa55e73d3a1a055518
Kajlongero <dev.kajlongero@gmail.com>
2025-08-07 17:22:54 -0400

    feat: adding a type to authcontext

--- apps/frontend/src/context/AuthContext/interface.ts
+++ apps/frontend/src/context/AuthContext/interface.ts
@@ -2,7 +2,10 @@ import type { User } from "../../types/user";
 
 export interface AuthContextInterface {
   user: User | null;
+  expiredToken: boolean;
 
   setUserData: (data: User) => void;
   clearUserData: () => void;
+
+  setExpiredToken: (expired: boolean) => void;
 }
