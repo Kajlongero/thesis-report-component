d40f1b1372d357070e65f3b7059e1f6e639d60cf
Kajlongero <dev.kajlongero@gmail.com>
2025-08-07 03:52:46 -0400

    feat: added persistence of the user data on the frontend with some dispatch functions

--- apps/frontend/src/context/AuthContext/index.tsx
+++ apps/frontend/src/context/AuthContext/index.tsx
@@ -6,17 +6,25 @@ import {
   authReducer,
   authReducerInitialState,
 } from "../../reducers/AuthReducer";
+import type { User } from "../../types/user";
 
 export const AuthContextProvider = ({
   children,
 }: {
   children: React.ReactNode;
 }) => {
-  const [auth] = useReducer(authReducer, authReducerInitialState);
+  const [auth, dispatch] = useReducer(authReducer, authReducerInitialState);
 
   const { user } = auth;
 
+  const setUserData = (data: User) =>
+    dispatch({ type: "SET_USER_DATA", payload: data });
+
+  const clearUserData = () => dispatch({ type: "CLEAR_USER_DATA" });
+
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, setUserData, clearUserData }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
--- apps/frontend/src/context/AuthContext/interface.ts
+++ apps/frontend/src/context/AuthContext/interface.ts
@@ -2,4 +2,7 @@ import type { User } from "../../types/user";
 
 export interface AuthContextInterface {
   user: User | null;
+
+  setUserData: (data: User) => void;
+  clearUserData: () => void;
 }
