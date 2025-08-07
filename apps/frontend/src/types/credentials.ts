322c2c4deeb93634dee39a976fb71229cba9989b
Kajlongero <dev.kajlongero@gmail.com>
2025-08-06 21:42:45 -0400

    feat: common types

---
+++ apps/frontend/src/types/credentials.ts
@@ -0,0 +1,11 @@
+export type LoginCredentials = {
+  email: string;
+  password: string;
+};
+
+export type RegisterCredentials = {
+  email: string;
+  password: string;
+  lastName: string;
+  firstName: string;
+};
