e8aa9ca18c4cac02bd0f6ef0efa6849a40493eff
Kajlongero <dev.kajlongero@gmail.com>
2025-08-07 03:53:19 -0400

    feat: added conditional render if user has or has not valid user data [still incomplete]

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,20 +1,42 @@
+import { ToastContainer } from "react-toastify";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+import { LoginPage } from "./pages/Login";
+import { RegisterPage } from "./pages/Register";
+
+function AuthenticatedLayout() {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <div>
+              <h1>Hola mundo</h1>
+            </div>
+          </>
+        }
+      />
+    </Routes>
+  );
+}
+
+function NotAuthenticatedLayout() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+    </Routes>
+  );
+}
+
 function AppLayout() {
+  const user = false;
+
   return (
     <div className="min-h-screen bg-background">
       <main className="flex-1 p-6">
-        <Routes>
-          <Route
-            index
-            path="/"
-            element={
-              <div>
-                <h1>Hola mundo</h1>
-              </div>
-            }
-          />
-        </Routes>
+        {user ? <AuthenticatedLayout /> : <NotAuthenticatedLayout />}
       </main>
     </div>
   );
@@ -23,6 +45,7 @@ function AppLayout() {
 export function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <AppLayout />
     </BrowserRouter>
   );
