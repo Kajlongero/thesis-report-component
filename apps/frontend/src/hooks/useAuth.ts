36753da93b6be385f0dfe6f683f06fbd73f04146
Kajlongero <dev.kajlongero@gmail.com>
2025-08-07 04:14:42 -0400

    feat: adding structure to add session validation and refresh token automatic handling

--- apps/frontend/src/hooks/useAuth.ts
+++ apps/frontend/src/hooks/useAuth.ts
@@ -1,20 +1,40 @@
+import { useLocation } from "react-router-dom";
 import { useEffect, useReducer } from "react";
 
+import { useFetch } from "./useFetch";
 import { authReducer, authReducerInitialState } from "../reducers/AuthReducer";
 
 import type { User } from "../types/user";
 
 export const useAuth = () => {
+  const location = useLocation();
   const [auth, dispatch] = useReducer(authReducer, authReducerInitialState);
+
+  const refreshSession = useFetch({
+    tx: "RefreshToken",
+    fnName: "fetch-refresh-token",
+  });
+  const validateSession = useFetch({
+    tx: "ValidateSession",
+    fnName: "fetch-valid-session",
+  });
+
+  const handleRefreshToken = async () => {};
+
+  const handleValidateSession = async () => {};
 
   const setUserData = (data: User) =>
     dispatch({ type: "SET_USER_DATA", payload: data });
 
   const clearUserData = () => dispatch({ type: "CLEAR_USER_DATA" });
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log("Fetch user data");
+  }, []);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    console.log(location.pathname);
+  }, [location.pathname]);
 
   return {
     user: auth.user,
