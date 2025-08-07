b65923a21d6b9500368ee4eea5453ed2dbbebebe
Kajlongero <dev.kajlongero@gmail.com>
2025-08-07 18:00:10 -0400

    feat: adding new conditions into the hook to make it more flexible

--- apps/frontend/src/hooks/useAuth.ts
+++ apps/frontend/src/hooks/useAuth.ts
@@ -1,15 +1,23 @@
-import { useLocation } from "react-router-dom";
 import { useEffect, useReducer } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { useFetch } from "./useFetch";
 import { authReducer, authReducerInitialState } from "../reducers/AuthReducer";
+import { authNotRequiredPaths, authRequiredPaths } from "../lib/protected";
 
 import type { User } from "../types/user";
+import type { ApiResponse } from "../types/api";
 
 export const useAuth = () => {
+  const navigate = useNavigate();
   const location = useLocation();
+
   const [auth, dispatch] = useReducer(authReducer, authReducerInitialState);
 
+  const getUserData = useFetch({
+    tx: "GetUserData",
+    fnName: "fetch-user-data",
+  });
   const refreshSession = useFetch({
     tx: "RefreshToken",
     fnName: "fetch-refresh-token",
@@ -19,26 +27,88 @@ export const useAuth = () => {
     fnName: "fetch-valid-session",
   });
 
-  const handleRefreshToken = async () => {};
+  const handleGetUserData = async () => {
+    const data = await getUserData.process(undefined);
+    const user = data.data as User;
 
-  const handleValidateSession = async () => {};
+    if (!data.error) dispatch({ type: "SET_USER_DATA", payload: user });
+  };
+
+  const handleRefreshToken = async () => {
+    const data = await refreshSession.process(undefined);
+    const result = data as ApiResponse<null>;
+
+    if (result.error) {
+      clearUserData();
+
+      navigate("/login");
+    }
+
+    return result;
+  };
+
+  const handleValidateSession = async () => {
+    const data = await validateSession.process(undefined);
+    const result = data as ApiResponse<null>;
+
+    const error = result.error;
+    const tokenExpired = result.message === "Token expired";
+    const statusCode = result.statusCode === 401;
+
+    if (error && tokenExpired && statusCode) return null;
+
+    return result;
+  };
+
+  const handleRefreshTokenAndFetchUser = async () => {
+    if (auth.expiredToken) return;
+
+    const result = await handleRefreshToken();
+
+    if (!result.error && !auth.user) await handleGetUserData();
+  };
+
+  const handleValidateSessionAndRedirect = async (route: string) => {
+    const data = await handleValidateSession();
+
+    if (!data) return;
+
+    if (authRequiredPaths.has(route) && data.error) {
+      clearUserData();
+
+      navigate("/login");
+    }
+
+    if (authNotRequiredPaths.has(route) && !data.error) {
+      navigate("/dashboard");
+    }
+  };
 
   const setUserData = (data: User) =>
     dispatch({ type: "SET_USER_DATA", payload: data });
 
+  const setExpiredToken = (expired: boolean) =>
+    dispatch({ type: "SET_EXPIRED_TOKEN", payload: expired });
+
   const clearUserData = () => dispatch({ type: "CLEAR_USER_DATA" });
 
   useEffect(() => {
-    console.log("Fetch user data");
+    handleGetUserData();
   }, []);
 
   useEffect(() => {
-    console.log(location.pathname);
+    handleRefreshTokenAndFetchUser();
+  }, [auth.expiredToken]);
+
+  useEffect(() => {
+    handleValidateSessionAndRedirect(location.pathname);
   }, [location.pathname]);
 
   return {
     user: auth.user,
+    expiredToken: auth.expiredToken,
     setUserData,
     clearUserData,
+    setExpiredToken,
   };
 };
