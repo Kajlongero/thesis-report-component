121e3c851b2bac1fee514a83b8df42e70f8b22e5
Kajlongero <dev.kajlongero@gmail.com>
2025-08-10 20:25:08 -0400

    feat: new type to fetch data from the backend, pagination with cursor

--- apps/frontend/src/types/api.ts
+++ apps/frontend/src/types/api.ts
@@ -4,3 +4,14 @@ export type ApiResponse<T> = {
   message: string;
   statusCode: number;
 };
+
+export type ApiCursorResponse<T> = {
+  data: T[];
+  message: string;
+  statusCode: number;
+  pagination: {
+    nextCursor: number;
+    hasNextPage: boolean;
+    totalElements: number;
+  };
+};
