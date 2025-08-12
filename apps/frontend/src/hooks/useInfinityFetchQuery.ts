87263d61817a241608a6a2af43629a45d2b4a046
Kajlongero <dev.kajlongero@gmail.com>
2025-08-12 01:04:52 -0400

    feat: hooks to infinity scroll queries

--- apps/frontend/src/hooks/useInfinityFetchQuery.ts
+++ apps/frontend/src/hooks/useInfinityFetchQuery.ts
@@ -3,10 +3,11 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { txsMapper } from "../utils/txs";
 import { fetchData } from "../utils/fetcher";
 
+import type { ApiCursorResponse } from "../types/api";
+
 interface UseInfiniteApiQueryProps<TParams> {
   tx: keyof typeof txsMapper;
   fnName: string;
-
   params?: TParams;
   options?: object;
 }
@@ -17,18 +18,24 @@ export const useInfiniteApiQuery = <TData, TParams = object>({
   params,
   options,
 }: UseInfiniteApiQueryProps<TParams>) => {
-  const queryKey = [fnName];
-  const queryFn = ({ pageParam }: { pageParam: unknown }) => {
+  const queryKey = [fnName, params];
+
+  const queryFn = async ({
+    pageParam,
+  }: {
+    pageParam: unknown;
+  }): Promise<ApiCursorResponse<TData>> => {
     const newParams = { ...params, cursor: pageParam };
-    return fetchData<TData>(tx, newParams);
+    const result = await fetchData<TData>(tx, newParams);
+    return result as unknown as ApiCursorResponse<TData>;
   };
 
   const query = useInfiniteQuery({
     queryKey,
     queryFn,
-    initialPageParam: 0,
-    getNextPageParam: (lastPage: any) => lastPage.data?.nextCursor,
-
+    initialPageParam: null,
+    getNextPageParam: (lastPage) => lastPage.pagination.nextCursor,
+    refetchOnMount: false,
     ...options,
   });
 
