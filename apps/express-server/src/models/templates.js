ca4c17a7f7eead037f957fe1a0dd57c95cacc45c
Kajlongero <dev.kajlongero@gmail.com>
2025-08-13 03:34:50 -0400

    feat: fixed cursors

--- apps/express-server/src/models/templates.js
+++ apps/express-server/src/models/templates.js
@@ -1,8 +1,8 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
-const limit = Joi.number().integer().min(1).max(50);
-const cursor = Joi.string();
 const id = Joi.number().integer().min(1);
+const limit = Joi.number().integer().min(1).max(50);
+const cursor = Joi.string().allow(null, "");
 
 const getAllTemplatesSchema = Joi.object({
   limit: limit,
