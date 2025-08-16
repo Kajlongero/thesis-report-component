83989c41a689fdca1ff292d284832c888f433676
Kajlongero <dev.kajlongero@gmail.com>
2025-08-16 19:37:21 -0400

    feat(context): add placeholders context

--- apps/frontend/src/context/index.ts
+++ apps/frontend/src/context/index.ts
@@ -3,6 +3,7 @@ import { createContext } from "react";
 import type { AuthContextInterface } from "./AuthContext/interface";
 import type { QuillContextInterface } from "./QuillContext/interface";
 import type { SidebarContextInterface } from "./SidebarContext/interface";
+import type { PlaceholdersContextInterface } from "./PlaceholdersContext/interface";
 
 export const AuthContext = createContext<AuthContextInterface>(
   {} as AuthContextInterface
@@ -14,4 +15,8 @@ export const SidebarContext = createContext<SidebarContextInterface>(
 
 export const QuillContext = createContext<QuillContextInterface>(
   {} as QuillContextInterface
+);
+
+export const PlaceholdersContext = createContext<PlaceholdersContextInterface>(
+  {} as PlaceholdersContextInterface
 );
