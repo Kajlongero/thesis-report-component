b32731bf846b5f49587bfb4da9a67493404a1c9f
Kajlongero <dev.kajlongero@gmail.com>
2025-08-16 19:38:30 -0400

    feat(types): update template types with templateDefinition

--- apps/frontend/src/types/templates.ts
+++ apps/frontend/src/types/templates.ts
@@ -1,10 +1,20 @@
+import type { DeltaStatic } from "react-quill-new";
+import type { Placeholder } from "./placeholders";
+
+export type TemplateDefinition = {
+  raw: string;
+  delta: DeltaStatic;
+  placeholders: Placeholder[];
+};
+
 export type Templates = {
   id: number;
   name: string;
-  description: string;
   isPublic: boolean;
   isActive: boolean;
   createdAt: string;
+  description: string;
   templateType: string;
   createdByName: string;
+  templateDefinition: TemplateDefinition | string;
 };
