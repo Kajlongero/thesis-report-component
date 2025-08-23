8ed16914e788e4ed7fa727778be39d850c54c2ad
Kajlongero <dev.kajlongero@gmail.com>
2025-08-23 18:09:29 -0400

    feat(cache): add queries and placeholders keys

--- apps/express-server/src/constants/cache.js
+++ apps/express-server/src/constants/cache.js
@@ -1,9 +1,12 @@
 const CACHE_KEYS = {
+  QUERIES: "queries",
   GRPC_RELATED: "grpcRelated",
   TRANSACTIONS: "transactions",
+  PLACEHOLDERS: "placeholders",
   OBJECT_AND_METHODS: "objectAndMethods",
   ROLE_METHOD_PERMISSIONS: "roleMethodPermissions",
   OBJECT_METHODS_INSTANCES: "objectMethodsInstances",
+  PLACEHOLDERS_WITH_QUERIES: "placeholdersWithQueries",
 };
 
 module.exports = CACHE_KEYS;
